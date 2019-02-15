200edf5f2f51b8f127f90180a7055f04a4892850
gouthamipoojari <47599459+gouthamipoojari@users.noreply.github.com>
2019-02-15 16:19:26 +0530

    Add files via upload

---
+++ great.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class great {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String value;
+        do{
+            System.out.println("enter a number");
+            value= scanner.nextLine();
+        }while(value == "A");
+        System.out.println("got 5!");
+    }
+}
+/hogreat.javame/nineleaps/IdeaProjects/project1
+        great.java
+        /home/nineleaps/IdeaProjects/project1/src/great.java
---
+++ non.java
@@ -0,0 +1,18 @@
+public class non {
+    public static void main(String[] args){
+//        String text = "hello";
+//        String blank =" ";
+//        String name ="bob";
+//        String greeting = text+blank+name;
+//        System.out.println(greeting);
+//        System.out.println("hello world"+" "+"my name is bob");
+        int value =0;
+        while(value<10)
+        {
+            System.out.println("hello" +value);
+            value = value+1;
+        }
+
+
+   }
+}
